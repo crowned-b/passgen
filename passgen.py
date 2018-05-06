dac95a115c09b522ef45e1abee204fc7170cce78
crowned-b <nathan@prouse.us>
2018-05-05 23:42:24 -0400

    Create passgen.py

---
+++ passgen.py
@@ -0,0 +1,38 @@
+import random
+import time
+
+def testStrength(newPassword, vectors, passwordLen):
+    for vector in vectors:
+        if not set(newPassword).intersection(set(vector)):
+            filler=random.randrange(passwordLen-1)
+            newPassword=newPassword[:filler] + vector[random.randrange(len(vector))] + newPassword[filler+1:]
+            return [newPassword,-1,vector]
+    return [newPassword,1,vector]
+
+def passwordGen(passwordLen):
+    alphaLow = "abcdefghijklmnopqrstuvwxyz"
+    alphaUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    num = "1234567890"
+    special = ":;>.<,?/|\_-+=!@#$%^&*()\"\\"
+    allChars=alphaLow+alphaUp+num+special
+    vectors=[alphaLow,alphaUp,num,special]
+    newPassword=""
+
+    for i in range(passwordLen):
+        newChar = random.choice(allChars)
+        newPassword+=newChar
+    while testStrength(newPassword, vectors, passwordLen)[1] == -1:
+        newPassword=testStrength(newPassword,vectors,passwordLen)[0]
+    return newPassword
+try:
+    passwordLen = int(raw_input("Please enter password length: "))
+    if passwordLen >= 8:
+        print("Your new password: %s" % passwordGen(passwordLen))
+    else:
+        print("Password too short. Using default of 8.")
+        print("Your new password: %s" % passwordGen(8))
+except ValueError:
+    print("Invalid length. Using default length of 8.\n")
+    print("Your new password: %s" % passwordGen(8))
+
+time.sleep(10)
